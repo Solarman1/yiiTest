0f394c0b1951fa91529a849457a8a81869517116
Solarman1 <arthurthehead@gmail.com>
2021-01-22 22:36:42 +0700

    loginWithPassCreate

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -14,16 +14,12 @@ class m130524_201442_init extends Migration
 
         $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
-            'username' => $this->string()->notNull()->unique(),
-            'auth_key' => $this->string(32)->notNull(),
-            'password_hash' => $this->string()->notNull(),
-            'password_reset_token' => $this->string()->unique(),
-            'email' => $this->string()->notNull()->unique(),
-
-            'status' => $this->smallInteger()->notNull()->defaultValue(10),
-            'created_at' => $this->integer()->notNull(),
-            'updated_at' => $this->integer()->notNull(),
+            'password' => $this->string()->notNull(),
         ], $tableOptions);
+
+        $this->insert('{{%user}}', [
+            'password' => 'qwerty123456',
+        ]);
     }
 
     public function down()
---
+++ console/migrations/m210122_153400_create_apples_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%apples}}`.
+ */
+class m210122_153400_create_apples_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%apples}}', [
+            'id' => $this->primaryKey(),
+            'color' => $this->text(),
+            'createDate' => $this->timestamp(),
+            'fallToGroundDate' => $this->dateTime(),
+            'appleStatus' => $this->integer(),
+            'eatingProcent' => $this->integer(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%apples}}');
+    }
+}
